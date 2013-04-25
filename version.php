28c2b14aee40cd99253aa4d37e644f21049488cc
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-04-26 10:34:30 +1200

    Permission fixes
    Managers need to be able to see reports; block needs to be able to be added to the site homepage so that it can be "sticky" throughout site.

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
-    $plugin->version = 201207241050;  // YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->version = 201207241053;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2010112400.00; // YYYYMMDDHH (This is the release version for Moodle 2.0)
     $plugin->cron = 900;
 ?>
