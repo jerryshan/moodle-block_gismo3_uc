10389543980604e361b94f1c86ddb7eab3bafef7
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-07-24 10:44:14 +1200

    Remove closing PHP tag from version.php

--- version.php
+++ version.php
@@ -2,4 +2,3 @@
     $plugin->version = 201207241053;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2010112400.00; // YYYYMMDDHH (This is the release version for Moodle 2.0)
     $plugin->cron = 900;
-?>
