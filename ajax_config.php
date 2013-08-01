7faef2c4cbb0bbd6b049f9f4a138ffabf5c0d71e
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-08-01 12:06:34 +1200

    Make Gismo options user-specific
    Since we use a site-wide instance of the block, instance-specific
    options are not at all useful.

--- ajax_config.php
+++ ajax_config.php
@@ -7,33 +7,42 @@
  * @copyright  eLab Christian Milani
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-// mode (josn)
-$error_mode = "json";
-
-// libraries & acl
-require_once "common.php";
-
-// query
+    // mode (josn)
+    $error_mode = "json";
+    
+    // libraries & acl
+    require_once "common.php";
+    global $USER;
+    
+    // query
 //$q = optional_param('q', '', PARAM_TEXT);
 $config_data = optional_param_array('config_data',array(),PARAM_INT);
-// decide what to do
-switch ($q) {
-    case "save":
-        $result = array("status" => "false");
+    // decide what to do
+    switch ($q) {
+        case "save":
+            $result = array("status" => "false");
         if (isset($config_data ) AND is_array($config_data ) AND count($config_data ) > 0) {
-            // serialize and encode config data
+                // serialize and encode config data
             $config_data_encode = base64_encode(serialize((object) $config_data ));
-            // update config
-            $check = $DB->set_field("block_instances", "configdata", $config_data_encode, array("id" => $srv_data->block_instance_id));
-            if ($check !== false) {
-                $result["status"] = "true";
+                // update config
+                if ($opts = $DB->get_record('block_gismo_user_options', array('user'=>$USER->id))) {
+                    $opts->configdata = $config_data;
+                    $check = $DB->update_record('block_gismo_user_options', $opts);
+                } else {
+                    $opts = new stdClass;
+                    $opts->user = $USER->id;
+                    $opts->configdata = $config_data;
+                    $check = $DB->insert_record('block_gismo_user_options', $opts);
+                }
+                if ($check !== false) {
+                    $result["status"] = "true";    
+                }    
             }
-        }
-        break;
-    default:
-        break;
-}
-
-// send response
-echo json_encode($result);
+            break;
+        default:
+            break;    
+    }
+    
+    // send response
+    echo json_encode($result);   
 ?>
--- common.php
+++ common.php
@@ -166,13 +166,14 @@ if ($gismoconfig->student_reporting === "false") {
 }
     // get gismo settings
 
-    $gismo_settings = $DB->get_field("block_instances", "configdata", array("id" => intval($srv_data->block_instance_id)));
+    global $USER;
 
-    if (is_null($gismo_settings) OR $gismo_settings === "") {
+    $gismo_settings = $DB->get_field("block_gismo_user_options", "configdata", array("user" => intval($USER->id)));
 
-    $gismo_settings = get_object_vars(block_gismo\GISMOutil::get_default_options());
+    if (empty($gismo_settings)) {
+        $gismo_settings = get_object_vars(block_gismo\GISMOutil::get_default_options());   
+
     } else {
-
         $gismo_settings = get_object_vars(unserialize(base64_decode($gismo_settings)));
 
         if (is_array($gismo_settings) AND count($gismo_settings) > 0) {
