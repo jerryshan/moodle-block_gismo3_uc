b0e7491013c5ca1f2f93859ce9c0f83c539862fa
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2015-12-02 09:17:37 +1300

    Add upgrade step to migrate from legacy logs to standard
    Rough matching from legacy logs to standard log store, based on timestamp.
    The newest record in the standard log store which matches the timestamp of
    the last exported log ID will be used, so some actions (those which happen
    within the same second as the last exported entry) may not be captured.

--- db/upgrade.php
+++ db/upgrade.php
@@ -7,7 +7,7 @@
  * @copyright  eLab Christian Milani
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-// This file keeps track of upgrades to 
+// This file keeps track of upgrades to
 // the moodle_gismo
 //
 // Sometimes, changes between versions involve
@@ -31,5 +31,20 @@ function xmldb_block_gismo_upgrade($oldversion = 0) {
     // $dbman = $DB->get_manager();
     // STUFFS HERE
 
+    if ($oldversion <= 2013101501) {
+        // Shift from legacy log store to standard log store.
+        $lastexport = $DB->get_record('block_gismo_config', array('name' => 'last_export_max_log_id'));
+        if (!empty($lastexport->value)) {
+            $legacy = $DB->get_record('log', array('id' => $lastexport->value));
+            $standard = $DB->get_records('logstore_standard_log', array('timecreated'=>$legacy->time), 'id DESC', 'id', 0, 1);
+            if ($standard) {
+                $standard = reset($standard);
+                $lastexport->value = $standard->id;
+                $DB->update_record('block_gismo_config', $lastexport);
+            }
+        }
+
+        upgrade_block_savepoint(true, 2013101501, 'gismo');
+    }
     return true;
 }
